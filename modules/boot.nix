7688f379889d20727e0e56b2c9bae9939fb4053d
ItsDoomOne <leodroid796@gmail.com>
2026-01-01 00:05:58 -0300

    Feliz ano novo! Adicionei novas funções ao Systemd

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,4 +1,6 @@
 { config, pkgs, ... }: {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.netbootxyz.enable = true;
+  boot.loader.systemd-boot.edk2-uefi-shell.enable = true;
 }
