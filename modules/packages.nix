21c1302473c0a66e03b1eb7bddc0ccaea9179321
Leonardo H. S. <85998702+ItsDoomOne@users.noreply.github.com>
2026-01-05 00:22:38 +0000

    Adicionado Brasero+Neovim e retirado diversos pacotes.

--- modules/packages.nix
+++ modules/packages.nix
@@ -7,24 +7,19 @@
   pkgs.nixpkgs-fmt
   pkgs.vscode
   pkgs.prismlauncher
-  pkgs.gzdoom
   pkgs.javaPackages.compiler.temurin-bin.jre-21
   pkgs.javaPackages.compiler.temurin-bin.jre-17
-  pkgs.javaPackages.compiler.temurin-bin.jre-8
-  pkgs.sonobus
   pkgs.vesktop
   pkgs.nerd-fonts.jetbrains-mono
-  pkgs.cemu
   pkgs.cmatrix
   pkgs.python315
   pkgs.mpv
   pkgs.wget
-  pkgs.vlc
-  pkgs.uxplay
   pkgs.shairport-sync
   pkgs.nqptp
-  pkgs.kdePackages.k3b
+  pkgs.brasero # brasero melhor que o k3b
   pkgs.cdrdao
   pkgs.dvdplusrwtools
+  pkgs.neovim
   ];
 }
